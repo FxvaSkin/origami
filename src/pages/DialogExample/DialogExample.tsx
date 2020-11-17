af1d708f6d6fb3bf051ffddcf66c4cc9225919cd
Fxva <fxva@outlook.com>
2020-11-17 22:58:06 +0600

    Добавил пример вложенного модала

--- src/pages/DialogExample/DialogExample.tsx
+++ src/pages/DialogExample/DialogExample.tsx
@@ -5,14 +5,26 @@ import { useBoolean } from 'hooks'
 interface Props {}
 
 const DialogExample: React.FC<Props> = () => {
-  const [isOpen, { setTrue: openDialog, setFalse: closeDialog }] = useBoolean(
-    false,
-  )
+  const [
+    isOpen1,
+    { setTrue: openDialog1, setFalse: closeDialog1 },
+  ] = useBoolean(false)
+  const [
+    isOpen2,
+    { setTrue: openDialog2, setFalse: closeDialog2 },
+  ] = useBoolean(false)
   return (
     <div>
-      <button onClick={openDialog}>Open Dialog</button>
-      <Dialog isOpen={isOpen} onClose={closeDialog}>
-        Dialog
+      <button onClick={openDialog1}>Open Dialog 1</button>
+      <Dialog isOpen={isOpen1} onClose={closeDialog1}>
+        <button onClick={openDialog2}>Open Dialog 2</button>
+        <Dialog
+          isOpen={isOpen2}
+          onClose={closeDialog2}
+          style={{ minWidth: '8em', minHeight: '7em' }}
+        >
+          Dialog 2
+        </Dialog>
       </Dialog>
     </div>
   )
